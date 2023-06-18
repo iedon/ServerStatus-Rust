605ef8c0e1c97703ae0500a4409661c336d46d6a
iEdon <m@iedon.net>
2023-06-19 04:23:12 +0900

    chore: change connectivity check url for rust-ver

--- client/src/status.rs
+++ client/src/status.rs
@@ -24,8 +24,8 @@ use stat_common::server_status::StatRequest;
 
 const SAMPLE_PERIOD: u64 = 1000; //ms
 const TIMEOUT_MS: u64 = 1000;
-static IPV4_ADDR: &str = "ipv4.google.com:80";
-static IPV6_ADDR: &str = "ipv6.google.com:80";
+static IPV4_ADDR: &str = "ip4.me:80";
+static IPV6_ADDR: &str = "ip6only.me:80";
 
 pub fn get_uptime() -> u64 {
     fs::read_to_string("/proc/uptime")
